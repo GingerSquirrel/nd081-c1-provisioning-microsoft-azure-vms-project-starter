d828a90cb6da46f6528c645ed69a1a6b8cfa421a
Harriet Lawrie <chocolatey_squirrel@hotmail.com>
2020-12-23 19:45:32 +0000

    Changed logging level

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -9,12 +9,11 @@ from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__)
-app.run(debug=True)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-app.logger.setLevel(logging.DEBUG)
+app.logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.DEBUG)
+streamHandler.setLevel(logging.INFO)
 app.logger.addHandler(streamHandler)
 
 Session(app)
