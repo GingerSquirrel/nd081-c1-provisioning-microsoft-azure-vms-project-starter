19596e4e2f0656ef58d3c70d6920d644d4d44763
Harriet Lawrie <chocolatey_squirrel@hotmail.com>
2020-12-24 01:36:59 +0000

    Changed logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -8,13 +8,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+handler = logging.StreamHandler(stream=sys.stdout)
+app.logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
-streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.INFO)
-app.logger.addHandler(streamHandler)
+app.logger.info('App startup')
+app.logger.info(header)
+
+
+
+# app.logger.setLevel(logging.INFO)
+# streamHandler = logging.StreamHandler()
+# streamHandler.setLevel(logging.INFO)
+# app.logger.addHandler(streamHandler)
 
 Session(app)
 db = SQLAlchemy(app)
--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -74,6 +74,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         app.logger.info('logged in successfully')
+        app.logger.error('logged in successfully')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
